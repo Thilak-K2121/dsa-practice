b074b026825a112abf94a4c7da52449e43edd605
Thilak-K2121 <thilakkdevadiga5@gmail.com>
2025-08-15 18:17:23 +0530

    Update (23)Merge k Sorted Lists.java

--- (23)Merge k Sorted Lists.java
+++ (23)Merge k Sorted Lists.java
@@ -1,3 +1,38 @@
+//So far the best approach using priority queue
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0) return null;
+
+        PriorityQueue<ListNode> pq= new PriorityQueue<>((a,b)->a.val-b.val);
+
+        for(ListNode node : lists){
+            if(node!=null){
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummy= new ListNode(-1);
+        ListNode tail=dummy;
+
+        while(!pq.isEmpty()){
+            ListNode smallest=pq.poll();
+            tail.next=smallest;
+            tail=tail.next;
+
+            if(smallest.next!=null){
+                pq.offer(smallest.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
+
+
+
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 //The merging head with other lists (beats 9%)
 class Solution {
      private static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
